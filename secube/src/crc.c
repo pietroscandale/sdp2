aad46ecd21ef679461176e1798e9f687472573e6
Pietro <pietro-1993@hotmail.it>
2018-04-19 01:09:02 +0200

    bjj

--- secube/src/crc.c
+++ secube/src/crc.c
@@ -6,7 +6,7 @@
   ******************************************************************************
   *
   * COPYRIGHT(c) 2016 STMicroelectronics
-  *
+  ***ehi
   * Redistribution and use in source and binary forms, with or without modification,
   * are permitted provided that the following conditions are met:
   *   1. Redistributions of source code must retain the above copyright notice,
